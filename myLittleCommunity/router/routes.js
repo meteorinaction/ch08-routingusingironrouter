58938f5758fb669644241243f88eff70220c8b17
Stephan Hochhaus <stephan@yauh.de>
2015-04-23 19:44:26 +0200

    Listing 8.22 RESTful routes

--- myLittleCommunity/router/routes.js
+++ myLittleCommunity/router/routes.js
@@ -20,4 +20,31 @@ Router.route('/api/profiles/name/:_id', function () {
   }).name);
 }, {
   where: 'server'
-})
+});
+Router.route('/api/find/profiles', {
+    where: 'server'
+  })
+  .get(function () {
+    this.response.statusCode = 200;
+    this.response.setHeader("Content-Type", "application/json");
+    this.response.setHeader("Access-Control-Allow-Origin", "*");
+    this.response.setHeader("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
+    this.response.end(JSON.stringify(
+      ProfilesCollection.find().fetch()));
+  })
+
+Router.route('/api/insert/profile', {
+    where: 'server'
+  })
+  .post(function () {
+    this.response.statusCode = 200;
+    this.response.setHeader("Content-Type", "application/json");
+    this.response.setHeader("Access-Control-Allow-Origin", "*");
+    this.response.setHeader("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept");
+    // returns ID for new profile
+    this.response.end(JSON.stringify(
+      ProfilesCollection.insert(this.request.body)
+    ));
+  })

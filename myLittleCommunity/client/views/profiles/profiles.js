cae1532e893122764f6609ab79c3cbe874488f91
Stephan Hochhaus <stephan@yauh.de>
2015-04-23 19:14:16 +0200

    use onRendered instead of deprecated rendered callback

--- myLittleCommunity/client/views/profiles/profiles.js
+++ myLittleCommunity/client/views/profiles/profiles.js
@@ -1,5 +1,5 @@
-Template.profileDetail.rendered = function () {
+Template.profileDetail.onRendered(function () {
   console.log("right side rendered");
   var template = Template.instance();
   template.$('h1').fitText();
-}
+})

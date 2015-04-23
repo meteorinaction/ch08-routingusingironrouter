82512ecb9162f0e37d53ab69eff5935ba1674ac0
Stephan Hochhaus <stephan@yauh.de>
2015-04-23 19:39:18 +0200

    Listing 8.21 Simple server-side route

--- myLittleCommunity/router/routes.js
+++ myLittleCommunity/router/routes.js
@@ -10,3 +10,14 @@ Router.route('/profiles/:_id', {
   controller: 'ProfileController',
   name: 'profile.details'
 });
+
+Router.route('/api/profiles/name/:_id', function () {
+  var request = this.request;
+  var response = this.response;
+
+  response.end(ProfilesCollection.findOne({
+    _id: this.params._id
+  }).name);
+}, {
+  where: 'server'
+})

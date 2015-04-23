d39acb79990e0a7de41ab97f1abad370fd1dfeae
Stephan Hochhaus <stephan@yauh.de>
2015-04-23 18:27:52 +0200

    Listing 8.7 Setting the data context with Iron.Router

--- myLittleCommunity/router/routes.js
+++ myLittleCommunity/router/routes.js
@@ -1,13 +1,31 @@
 Router.route('/', function () {
-  this.render('home');
+  this.render('home', {
+    data: function () {
+      return {
+        profiles: ProfilesCollection.find()
+      };
+    }
+  });
 });
+
 Router.route('/about', function () {
   this.render('about');
 });
-Router.route('/profiles/manuel', function () {
-  this.layout('profileLayout');
-  this.render('profileImage', {
-    to: 'left'
+
+Router.route('/profiles/:_id', function () {
+  profile = ProfilesCollection.findOne({
+    _id: this.params._id
   });
-  this.render('profileDetail');
+  this.layout('profileLayout');
+  this.render('profileDetailLeft', {
+    to: 'left',
+    data: function () {
+      return profile;
+    }
+  });
+  this.render('profileDetail', {
+    data: function () {
+      return profile;
+    }
+  });
 });
